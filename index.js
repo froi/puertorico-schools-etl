91d67a49752680d46d2eeffe0bff1201965ec634
Froilan Irizarry <froilan@froilanirizarry.me>
2018-04-12 21:02:48 -0400

    Add elasticsearch functionality - Added index verification - Added index creation - Added documents to index - Added elasticsearch client

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const fetch = require('node-fetch')
 const { School, StandardTestScores, getSchoolEnrollmentType, Geolocation } = require('./libs/models')
+const elasticsearch = require('elasticsearch')
+const { verifyIndexExists, createIndex, insertDocument, deleteIndex } = require('./libs/utils')
 
-fetch('https://data.pr.gov/resource/ceib-pqg3.json')
+const client = elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'trace'
+})
+
+verifyIndexExists('pr_schools', client)
+  .then(indexExists => {
+    if(indexExists) {
+      return deleteIndex('pr_schools', client)
+    }
+    return createIndex('pr_schools', client)
+  })
+  .then(() => fetch('https://data.pr.gov/resource/ceib-pqg3.json'))
   .then(response => response.json())
-  .then(data => {
+  .then(data => { // Transform data into our models
     const schoolList = []
     for(item of data) {
       let enrollment
@@ -20,5 +34,18 @@ fetch('https://data.pr.gov/resource/ceib-pqg3.json')
       schoolList.push(school)
     }
 
-    console.log(schoolList[0])
-  });
+    return schoolList
+  })
+  .then(schoolList => {
+    schoolList.forEach(school => {
+      insertDocument(school, 'pr_schools', 'school', client)
+        .then(response => console.log(response))
+        .then(() => 'Done!')
+        .catch(error => console.error(error))
+    })
+  })
+  .catch(error => console.error('ERROR:', error))
+   
+
+    
+  
